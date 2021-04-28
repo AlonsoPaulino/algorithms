0bff568c75f677d4cbe1d58d42b4fb3f8861dc00
Luis Alonso Paulino Flores <alonsopf1@gmail.com>
2021-04-28 01:02:06 -0500

    fix comments

--- Graphs/dfs.cpp
+++ Graphs/dfs.cpp
@@ -1,17 +1,15 @@
+// Depth First Search O(V + E)
 #include "../macros.h"
 
 const int N = 100;
-
-vi ady[N];
+vi ady[N]; // Adjacency list
 vector<bool> vis(N);
 
 // Recursive Approach
-// u: root node to start the dfs
 void dfs(int u) {
-    vis[u] = true; // Node u is set as visited
+    vis[u] = true;
     
     for (auto v: ady[u]) {
-        // If not visited, run dfs with v as root
         if (!vis[v]) {
             dfs(v);
         }
@@ -19,19 +17,15 @@ void dfs(int u) {
 }
 
 // Iterative approach
-// u: root node to start the dfs
 void dfs(int u) {
     stack<int> st;
     st.push(u);
 
     while (!st.empty()) {
-        // Get top node in the stack and remove it.
         int x = st.top(); st.pop();
-        // Node u is set as visited
         vis[x] = true;
 
         for (auto v: ady[u]) {
-            // If not visited, add to the stack
             if (!vis[v]) {
                 st.push(v);
             }
